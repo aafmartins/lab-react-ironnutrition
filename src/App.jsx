2fad11d6bea2222250ab1d274b058d0d0dc74db6
Ana Martins <ana.af.martins@gmail.com>
2021-09-22 17:15:33 +0200

    lab done
    Co-authored-by: monikageiger <monikageiger@users.noreply.github.com>

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import "./App.css";
+import foodsData from "./foods.json";
+import { useState } from "react";
+import { Row, Divider } from "antd";
+import FoodBox from "./components/FoodBox";
+import Search from "./components/Search";
+import AddFoodForm from "./components/AddFoodForm";
+
+function App() {
+  const [foods, setFoods] = useState(foodsData);
+  const [foodsDataCopy, setFoodsDataCopy] = useState(foodsData);
+
+  const addNewFood = (newFood) => {
+    const updatedFoods = [...foods, newFood];
+    const updatedFoodsDataCopy = [...foodsDataCopy, newFood];
+
+    setFoods(updatedFoods);
+    setFoodsDataCopy(updatedFoodsDataCopy);
+  };
+
+  const filterFoodList = (str) => {
+    let filteredFood;
+    if (str === "") {
+      filteredFood = foodsDataCopy;
+    } else {
+      filteredFood = foodsDataCopy.filter((food) => {
+        return food.name.toLowerCase().includes(str.toLowerCase());
+      });
+    }
+    console.log(filteredFood.length);
+    console.log(filteredFood);
+    setFoods(filteredFood);
+  };
+
+  //Iteration 1
+  // const foodsList = foods.map((food) => {
+  //   return (
+  //     <div>
+  //       <p>{food.name}</p>
+  //       <img src={food.image} alt="" width="100px" />
+  //     </div>
+  //   );
+  // });
+  const handleDelete = (foodName) => {
+    console.log("event:", foodName);
+    const filterDeletedFood = foodsDataCopy.filter((food) => {
+      return food.name !== foodName;
+    });
+    setFoods(filterDeletedFood);
+  };
+
+  return (
+    <div className="App">
+      <AddFoodForm addNewFood={addNewFood} />
+
+      <Search filterFoodList={filterFoodList} />
+      <Divider>Food List</Divider>
+
+      <Row style={{ width: "100%", justifyContent: "center" }}>
+        {foods.length === 0 ? (
+          <div>
+            <p>OOPS!</p>
+          </div>
+        ) : (
+          <>
+            {foods.map((food) => {
+              return (
+                <FoodBox
+                  handleDelete={() => handleDelete(food.name)}
+                  key={food.name}
+                  food={food}
+                />
+              );
+            })}
+          </>
+        )}
+      </Row>
+    </div>
+  );
+}
+
+export default App;
